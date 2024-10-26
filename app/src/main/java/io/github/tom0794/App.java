b2fc12e77ea0420ee548e1cc8987c959da4afb9d
Tom Chiasson <thom.chiasson@gmail.com>
2024-10-26 10:42:04 -0400

    Create basic project

---
+++ app/src/main/java/io/github/tom0794/App.java
@@ -0,0 +1,13 @@
+package io.github.tom0794;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
